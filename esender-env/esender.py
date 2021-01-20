7c8e063748c885024d9294fa6f0881dc88433583
caleby <caleby>
2021-01-20 23:01:57 +0800

    working email sender and message.txt

--- esender-env/esender.py
+++ esender-env/esender.py
@@ -1,12 +1,16 @@
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+from email.message import EmailMessage
 import smtplib
 
 mail_content = "Hello, this is a test email"
 smtphost = 'smtp.mail.yahoo.com'
 sender_email = 'cykp11@yahoo.com.sg'
 sender_pw = 'hcsxjloxlclhpejl'
+receiver_email = 'caleby117@gmail.com'
 
+with open('message.txt') as m:
+    msgtxt = m.read()
+
+message = EmailMessage()
 
 try: 
     server = smtplib.SMTP_SSL(smtphost, 465)
@@ -16,3 +20,25 @@ try:
 
 except Exception as e: 
     print(e)
+
+server.set_debuglevel(1)
+print(server.helo_resp)
+
+message['Subject'] = "Sent from Python"
+message['To'] = receiver_email
+message['From'] = sender_email
+message.set_content(msgtxt)
+
+print(message.keys())
+
+while True:
+    confirm = input(f"Sending email from {sender_email} to {receiver_email}, subject {message['Subject']}\n\nPress 'y' to confirm.")
+    if confirm == 'y':
+        break
+try:
+    server.send_message(message)
+
+except smtplib.SMTPSenderRefused:
+    print("SMTP SenderRefused" + message['sender'])
+
+server.quit()
